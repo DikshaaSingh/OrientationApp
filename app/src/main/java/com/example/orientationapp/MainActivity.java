68623292973819308dc040102a9552f994592295
Diksha Singh <DikshaSin@hcl.com>
2019-05-24 12:41:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/orientationapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.orientationapp;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
